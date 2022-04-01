58fb32a6d8550426c699cd1a24237a845fee5bd4
hoainam <nhnam1632@gmail.com>
2022-04-01 15:23:04 +0700

    update function deleted

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import {  PrismaClient } from '@prisma/client'
-import { PrismaClient as PrismaUserCore, User } from '../prisma/generator/clientUserCore'
-import _ = require('lodash');
+import { PrismaClient as PrismaUserCore } from '../prisma/generator/clientUserCore'
+import _ = require('lodash');   
 const prisma = new PrismaClient({
     datasources:{
         db:{
@@ -15,19 +15,30 @@ const prismaUser = new PrismaUserCore({
         }
     }
 })
-const getUsers = async (userIds)=>{
-    const users = await prismaUser.user.findMany({
-        where:{
-            id:{
-                in: userIds
-            }
-        },
-    })
-    const getUsers = _.keyBy(users, "id")
-    return getUsers
-}
+prisma.$use(async (params, next) => {
+    // Check incoming query type
+    if(params.action == "delete" && params.model !== "ProjectLikes" && params.model !== "ProjectInterested"){ 
+            const now = new Date()
+            params.action = "update"
+            params.args["data"] = { deleted: now }
+    }            
+    if(params.action == "findMany"){
+        if(params.args.where){
+            params.args.where.deleted =  null
+        }
+        else{
+            params.args["where"] = {deleted: null}
+        }
+    }
+    if(params.action == "findFirst" || params.action == "findUnique"){
+        params.action = "findFirst"
+        params.args.where.deleted = null
+    }
+    
+    return next(params)
+  });
+
 module.exports = {
     prisma,
     prismaUser,
-    getUsers
 }
