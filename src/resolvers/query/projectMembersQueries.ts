b6efab73b90bd1a1194fe42e092a9d70cf5cca4e
hoainam <nhnam1632@gmail.com>
2022-04-06 14:18:01 +0700

    update project_member

--- src/resolvers/query/projectMembersQueries.ts
+++ src/resolvers/query/projectMembersQueries.ts
@@ -33,20 +33,6 @@ export default {
                         projectId: +args.projectId
                     },
                 })
-                // const getIdMember = _.difference(_.map(detailMember, 'memberUserId'), [null])
-                // const userFeedback = await prisma.userFeedback.findMany({
-                //     where: {
-                //         userId: {
-                //             in: getIdMember
-                //         },
-                //         projectId: +args.projectId
-                //     }
-                // })
-                // const setKeyUserFeedback = _.keyBy(userFeedback, "userId")
-                // _.map(detailMember, (member) => {
-                //     member.userFeedback = setKeyUserFeedback[member.memberUserId]
-                //     return member
-                // })
                 return detailMember
             } catch (e) {
                 console.log(e)
@@ -55,25 +41,20 @@ export default {
         },
         detailProjectMemberByIdPm: async (parent, args, context) => {
             try {
+                var { userId } = context              
+                if(+args?.pmUserId){
+                    userId = +args.pmUserId
+                }
                 const searchProjectMember = await prisma.projectMembers.findMany({
                     distinct: ['projectId'],
                     where: {
-                        memberUserId: +args.pmUserId,
+                        memberUserId: userId,
                         position: 'pm'
                     }
                 })
                 if (searchProjectMember.length === 0) {
                     return null
                 }
-
-
-                // if(searchProjectMember === null){
-                //     return null     
-                // }
-                // var query = `SELECT * FROM project_members WHERE pm_user_id = ${args.pmUserId} GROUP BY project_id;`
-                // // var query = `SELECT project_id FROM project_members WHERE pm_user_id = ${args.pmuserId}`
-                // // console.log(query)
-                // const searchProjectMember = await prisma.$queryRawUnsafe(query)
                 return searchProjectMember
             }
             catch (e) {
